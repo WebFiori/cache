6daec96c3c17a9179065af0aca8052594bf620a9
Ibrahim <ibinshikh@hotmail.com>
2024-12-08 17:44:14 +0300

    test: Fix Autoload Path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,3 @@
 <?php
 
-require_once '..'.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
+require_once __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
